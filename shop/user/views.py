ec99001222f1230df2ca971c44859c88ec8a7fdc
Sharoon Thomas <sharoon.thomas@fulfil.io>
2016-08-30 10:26:26 -0400

    Add a dummy address book view

--- shop/user/views.py
+++ shop/user/views.py
@@ -7,12 +7,12 @@ from shop.utils import render_theme_template as render_template
 
 blueprint = Blueprint(
     'user', __name__,
-    url_prefix='/users', static_folder='../static'
+    url_prefix='/my', static_folder='../static'
 )
 
 
-@blueprint.route('/')
+@blueprint.route('/addresses')
 @login_required
-def members():
-    """List members."""
-    return render_template('users/members.html')
+def addresses():
+    """List Addresses."""
+    return render_template('users/addresses.html')
