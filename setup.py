6009990f292c384e3cf54f5dd678ad1dbb0308e2
Sharoon Thomas <sharoon.thomas@fulfil.io>
2016-08-30 07:37:40 -0400

    Structure as a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Fulfil-Shop',
+    version='0.1dev',
+    packages=['shop'],
+    license='BSD',
+    include_package_data=True,
+    zip_safe=False,
+    long_description=open('README.rst').read(),
+)
